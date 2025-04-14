9b3cb930de502ddba4c949978f87d03bc3fa1ae1
Andre Ruan <andreruan80@gmail.com>
2025-04-14 18:58:28 -0300

    Feat: :tada: Adicionando configurações do projeto

---
+++ electron/main.cjs
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
