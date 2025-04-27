f716eb00a48e71c8a10cb3e84daa3bbed096c393
Andre Ruan <andreruan80@gmail.com>
2025-04-26 23:46:44 -0300

    Refactor: Melhorias no component GenericCard

--- src/components/GenericCard/index.tsx
+++ src/components/GenericCard/index.tsx
@@ -2,22 +2,60 @@ import { Paper, Typography, Box } from "@mui/material";
 
 type GenericCardType = {
   title: string;
-  subtitle: string;
+  subtitle?: string;
   bgColor?: string;
+  border?: string;
+  displayCardStyle?: string;
+  flexDirectionCard?: "column" | "row";
+  gapCard?: string;
+  widthCard?: string;
+  minWidthCard?: string;
+  heightCard?: string;
+  minHeightCard?: string;
+  paddingCard?: number;
+  icon?: React.ReactNode;
+  bgColorIcon?: string;
   children?: React.ReactNode;
+  onClick?: () => void;
 };
 
-const GenericCard = ({ title, subtitle, bgColor, children }: GenericCardType) => {
+const GenericCard = ({
+  title,
+  subtitle,
+  bgColor,
+  border,
+  widthCard,
+  minWidthCard,
+  heightCard,
+  minHeightCard,
+  paddingCard,
+  displayCardStyle,
+  flexDirectionCard,
+  gapCard,
+  icon,
+  bgColorIcon,
+  children,
+  onClick,
+}: GenericCardType) => {
   return (
     <Paper
+      onClick={onClick}
       elevation={3}
       sx={{
-        p: 3,
+        p: paddingCard ?? 3,
+        display: displayCardStyle ?? "block",
+        flexDirection: flexDirectionCard ?? "",
+        gap: gapCard ?? "0",
         borderRadius: 2,
         backgroundColor: bgColor || "#1e2533",
-        width: "33rem",
+        border: border ?? "none",
+        width: widthCard ?? "33rem",
+        height: heightCard ?? "auto",
+        minWidth: minWidthCard ?? widthCard ?? "20rem",
+        minHeight: minHeightCard ?? heightCard ?? "20rem",
         transition: "0.3s",
         cursor: "pointer",
+        backgroundImage: "none",
         "&:hover": {
           boxShadow: 6,
           transform: "translateY(-5px)",
@@ -25,19 +63,19 @@ const GenericCard = ({ title, subtitle, bgColor, children }: GenericCardType) =>
       }}
     >
       <Box display="flex" alignItems="center" gap={2}>
-        {children && (
+        {icon && (
           <Box
             sx={{
-              bgcolor: "#2aaefc",
-              width: 45,
-              height: 45,
+              bgcolor: bgColorIcon ?? "#2aaefc",
+              width: 50,
+              height: 50,
               borderRadius: "50%",
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
             }}
           >
-            {children}
+            {icon}
           </Box>
         )}
 
@@ -45,11 +83,14 @@ const GenericCard = ({ title, subtitle, bgColor, children }: GenericCardType) =>
           <Typography fontWeight={700} color="white" fontSize="2rem">
             {title}
           </Typography>
-          <Typography variant="body2" fontSize="1.5rem" color="var(--title-gray-dark)">
-            {subtitle}
-          </Typography>
+          {subtitle && (
+            <Typography variant="body2" fontSize="1.5rem" color="var(--title-gray-dark)">
+              {subtitle}
+            </Typography>
+          )}
         </Box>
       </Box>
+      <Box>{children}</Box>
     </Paper>
   );
 };
