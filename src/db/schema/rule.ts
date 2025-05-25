49445e77f889ba338a28ef07931110ced983840d
Andre Ruan <andreruan80@gmail.com>
2025-05-24 21:14:31 -0300

    Feat: Adição da coluna isSystem para regras do sistema

--- src/db/schema/rule.ts
+++ src/db/schema/rule.ts
@@ -5,6 +5,7 @@ export const rule = sqliteTable("rule", {
   name: text("name").notNull().unique(),
   description: text("description", { length: 150 }),
   extensions: text("extensions"),
+  isSystem: integer("is_system", { mode: "boolean" }).notNull().default(false),
   isActive: integer("is_active", { mode: "boolean" }).notNull().default(true),
 });
 
