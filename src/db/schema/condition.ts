95efe795874bf9e1bf87c81fca5f16ee7bec9e38
Andre Ruan <andreruan80@gmail.com>
2025-05-24 21:14:04 -0300

    Feat: Adição das colunas type e typeAction com enums para tipar

--- src/db/schema/condition.ts
+++ src/db/schema/condition.ts
@@ -1,15 +1,21 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 import { rule } from "../schema/rule";
+import { ConditionsType } from "~/src/renderer/src/types/ConditionsType";
 
-export const condition = sqliteTable("condition", {
+export const conditionTable = sqliteTable("condition", {
   id: integer("id").primaryKey().unique(),
-  type: text("name").notNull(),
-  description: text("description"),
+  type: text("type", {
+    enum: ["fileName", "fileExtension", "modifiedDate", "fileDirectory", "fileLength"],
+  }).notNull(),
+  typeAction: text("type_action", {
+    enum: ["contains", "notContains", "startsWith", "endsWith", "equals"],
+  }).notNull(),
+  value: text("value"),
   ruleId: integer("rule_id")
     .references(() => rule.id, { onDelete: "cascade" })
     .notNull(),
 });
 
-export type Condition = typeof condition.$inferSelect;
-export type NewCondition = typeof condition.$inferInsert;
+export type Condition = typeof conditionTable.$inferSelect;
+export type NewCondition = Omit<typeof conditionTable.$inferInsert, "type" | "typeAction"> & ConditionsType;
