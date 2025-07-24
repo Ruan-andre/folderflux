ce83682b9bc6a73344839d1eca0df61c5874f6b3
Andre Ruan <andreruan80@gmail.com>
2025-07-24 20:27:38 -0300

    Feat: :tada: Adiciona novo schema para a lista de pastas que serão monitoradas

---
+++ src/db/schema/folders.ts
@@ -0,0 +1,15 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { ProfileFoldersTable } from "./profileFolders";
+import { relations } from "drizzle-orm";
+
+export const FolderTable = sqliteTable("folders", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  fullPath: text("full_path", { length: 150 }).notNull().unique(),
+});
+
+export const folderRelations = relations(FolderTable, ({ many }) => ({
+  profileFolders: many(ProfileFoldersTable),
+}));
+export type FolderSchema = typeof FolderTable.$inferSelect;
+export type NewFolder = typeof FolderTable.$inferInsert;
