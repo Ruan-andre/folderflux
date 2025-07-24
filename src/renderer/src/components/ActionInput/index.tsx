f7440042199c9e2ef90ad093c57f380848bad930
Andre Ruan <andreruan80@gmail.com>
2025-07-24 20:42:15 -0300

    Fix: Correções no componente de ação

--- src/renderer/src/components/ActionInput/index.tsx
+++ src/renderer/src/components/ActionInput/index.tsx
@@ -1,37 +1,51 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
 import GenericInput from "../GenericInput";
 import GenericFolderSelector from "../GenericFolderSelector";
+import { Action, NewAction } from "~/src/db/schema";
 
-const options = [
-  { label: "Mover para pasta", value: 0 },
-  { label: "Copiar para pasta", value: 1 },
-  { label: "Renomear arquivo", value: 2 },
-  { label: "Excluir arquivo", value: 3 },
+type ActionInputProps = {
+  action: Action | NewAction;
+  onChange: (updated: Action | NewAction) => void;
+};
+
+type optionsType = {
+  label: string;
+  value: "move" | "copy" | "rename" | "delete";
+};
+const options: optionsType[] = [
+  { label: "Mover para pasta", value: "move" },
+  { label: "Copiar para pasta", value: "copy" },
+  { label: "Renomear arquivo", value: "rename" },
+  { label: "Excluir arquivo", value: "delete" },
 ];
-const ActionInput = () => {
-  const [action, setAction] = useState(0);
-  const [destination, setDestination] = useState("");
 
+const ActionInput = ({ action, onChange }: ActionInputProps) => {
+  const handleChangeOption = (typeSelected: string) => {
+    onChange({ ...action, type: typeSelected as optionsType["value"] });
+  };
+
+  const handleValueChange = (value: string) => {
+    onChange({ ...action, value: value });
+  };
   return (
     <Box display="flex" flexDirection="column" gap={1} mt={2}>
       <GenericInput
-        name="options"
+        id="inputOptions"
+        name="inputOptions"
         select
         required
         selectOptions={options}
         label="Ação"
-        value={action.toString()}
-        onChange={(e) => setAction(parseInt(e.target.value))}
+        value={action?.type ?? "move"}
+        onChange={(e) => handleChangeOption(e.target.value)}
       />
-      {action !== 3 && (
+      {action?.type !== "delete" && (
         <GenericFolderSelector
           flexDirection="column"
-          showList={false}
-          btnAdd={false}
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-          placeholder={action === 2 ? "Novo nome" : "Pasta de destino"}
+          onChange={(e) => handleValueChange(e.target.value)}
+          value={action?.value ?? ""}
+          placeholder={action?.type === "rename" ? "Novo nome" : "Pasta de destino"}
+          hideSearchButton={action?.type === "rename"}
         />
       )}
     </Box>
