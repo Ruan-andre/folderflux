851913f9ef624a3778cc86a23920bf1055c0ca38
Andre Ruan <andreruan80@gmail.com>
2025-07-24 20:41:26 -0300

    Feat: Adição do ThemeProvider, CssBaseline e ConfirmDialogProvider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./assets/css/globals.css";
 import { SnackbarProvider } from "./context/SnackBarContext";
+import { ConfirmDialogProvider } from "./context/ConfirmDialogContext";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { GeneralTheme } from "./themes/GeneralTheme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
+    <ThemeProvider theme={GeneralTheme}>
+      <CssBaseline />
+      <ConfirmDialogProvider>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </ConfirmDialogProvider>
+    </ThemeProvider>
   </StrictMode>
 );
