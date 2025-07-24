d569a2b04594ccdd066d0dfa232462b225f0dcbc
Andre Ruan <andreruan80@gmail.com>
2025-07-24 20:16:09 -0300

    Feat: :tada: Cria tipo DbOrTx para utilizar a instância do banco em métodos/funções

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import path from "path";
 import { app } from "electron";
 import fs from "fs";
+import * as schema from "../db/schema";
 
 const dbPath =
   process.platform === "darwin"
@@ -11,14 +12,11 @@ const dbPath =
     : path.join(app.getPath("userData"), "tidyflux.db");
 
 const sqlite = new Database(dbPath);
-export const db = drizzle(sqlite);
+export const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, { schema });
 
-// interface SqliteTable {
-//   name: string;
-//   type: "table" | "index" | "view";
-//   tbl_name?: string;
-//   sql?: string;
-// }
+export type DbInstance = typeof db;
+export type Transaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
+export type DbOrTx = DbInstance | Transaction;
 
 export async function runMigrations() {
   const migrationsDir = path.join(__dirname, "db/migrations");
