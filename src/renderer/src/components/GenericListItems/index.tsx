8c5fcca9079f6f7939515270bc21fea92f850358
Andre Ruan <andreruan80@gmail.com>
2025-07-27 15:07:10 -0300

    Refactor: Refatoração do componente de listas genéricas para melhor uso nos demais componentes

--- src/renderer/src/components/GenericListItems/index.tsx
+++ src/renderer/src/components/GenericListItems/index.tsx
@@ -7,11 +7,18 @@ import {
   ListItemText,
   useTheme,
   Box,
+  Checkbox,
 } from "@mui/material";
 import GenericListItemsType from "../../types/GenericListItemsType";
+import Icon from "../../assets/icons";
+import CustomSwitch from "../CustomSwitch";
 
 interface GenericListItemsProps {
-  list: GenericListItemsType[];
+  list: GenericListItemsType[] | undefined;
+  mode?: "page" | "selection";
+  btnEdit?: boolean;
+  btnDelete?: boolean;
+  btnSwitch?: boolean;
   isButton?: boolean;
   titleSize?: string;
   subtitleSize?: string;
@@ -19,10 +26,16 @@ interface GenericListItemsProps {
   listItemPadding?: string;
   maxListHeight?: string;
   borderBottom?: string;
+  selectedIds?: number[];
+  onClickEdit?: (id: number) => void;
+  onClickDelete?: (id: number) => void;
+  onToggle?: (id: number) => void;
+  onSelectionChange?: (id: number) => void;
 }
 
 const GenericListItems = ({
   list,
+  mode = "page",
   isButton = true,
   titleSize,
   subtitleSize,
@@ -30,15 +43,21 @@ const GenericListItems = ({
   listItemPadding,
   maxListHeight,
   borderBottom,
+  btnEdit,
+  btnDelete,
+  btnSwitch,
+  selectedIds,
+  onClickDelete,
+  onClickEdit,
+  onSelectionChange,
 }: GenericListItemsProps) => {
   const theme = useTheme();
 
-  if (list.length === 0) return null;
+  if (!list || list.length === 0) return null;
 
   return (
     <List
       sx={{
-      
         width: "100%",
         overflowY: "auto",
         overflowX: "hidden",
@@ -54,20 +73,36 @@ const GenericListItems = ({
           }}
           disableGutters={noGutters ?? false}
           secondaryAction={
-            item.iconsAction && (
+            (btnEdit || btnDelete) && (
               <Box style={{ display: "flex", gap: "1rem" }}>
-                {item.iconsAction.map((icon, iconIndex) => (
-                  <IconButton key={iconIndex} edge="end" aria-label="Ação">
-                    {icon}
+                {btnEdit && onClickEdit && (
+                  <IconButton onClick={() => onClickEdit(item.id)}>
+                    <Icon icon="icon-park:edit-two" width="30" height="30" />
                   </IconButton>
-                ))}
+                )}
+                {btnDelete && onClickDelete && (
+                  <IconButton edge="end" onClick={() => onClickDelete(item.id)}>
+                    <Icon icon="material-icon-theme:folder-trash-open" width="30" height="30" />
+                  </IconButton>
+                )}
               </Box>
             )
           }
         >
+          {mode === "selection" && onSelectionChange && (
+            <Checkbox
+              checked={selectedIds?.some((x) => x === item.id)}
+              onChange={() => onSelectionChange(item.id)}
+            />
+          )}
           {isButton ? (
             <ListItemButton sx={{ borderRadius: theme.shape.borderRadius }}>
-              {item.icon && <ListItemIcon>{item.icon}</ListItemIcon>}
+              {item.icon && (
+                <ListItemIcon>
+                  <Icon icon={item.icon} width="30" height="30" />
+                </ListItemIcon>
+              )}
+
               <ListItemText
                 sx={{
                   "& .MuiListItemText-primary": {
@@ -83,7 +118,11 @@ const GenericListItems = ({
             </ListItemButton>
           ) : (
             <>
-              {item.icon && <ListItemIcon>{item.icon}</ListItemIcon>}
+              {item.icon && (
+                <ListItemIcon>
+                  <Icon icon={item.icon} width="30" height="30" />
+                </ListItemIcon>
+              )}
               <ListItemText
                 sx={{
                   "& .MuiListItemText-primary": {
@@ -98,6 +137,7 @@ const GenericListItems = ({
               />
             </>
           )}
+          {btnSwitch && <CustomSwitch checked />}
         </ListItem>
       ))}
     </List>
