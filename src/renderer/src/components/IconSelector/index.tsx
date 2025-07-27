03007f6cd74e2203cfbc1c93edcfcb6a747be1ac
Andre Ruan <andreruan80@gmail.com>
2025-07-27 15:07:40 -0300

    Refactor: Refatoração do iconSelector para uso nos demais componentes

--- src/renderer/src/components/IconSelector/index.tsx
+++ src/renderer/src/components/IconSelector/index.tsx
@@ -1,37 +1,31 @@
 import { useState } from "react";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material";
-import { Icon } from "@iconify/react";
+import Icon from "../../assets/icons/";
+import CommonIcons from "../../types/CommonIconsType";
 
-type IconOption = {
-  value: string;
-  icon: string;
-};
-
-export default function IconSelector() {
-  const [selected, setSelected] = useState("home");
+export default function IconSelector({
+  iconName,
+  onChangeIcon,
+}: {
+  iconName: string;
+  onChangeIcon: (iconId: string) => void;
+}) {
+  const [selected, setSelected] = useState(iconName ?? "fluent-color:home-32");
 
   const handleChange = (event: React.MouseEvent<HTMLElement>, newValue: string | null) => {
     event.preventDefault();
     if (newValue !== null) {
       setSelected(newValue);
+      onChangeIcon(newValue);
     }
   };
 
-  const iconOptions: IconOption[] = [
-    { value: "home", icon: "fluent-color:home-32" },
-    { value: "briefcase", icon: "noto:briefcase" },
-    { value: "smartphone", icon: "flat-color-icons:two-smartphones" },
-    { value: "music", icon: "emojione-v1:music-descend" },
-    { value: "camera", icon: "noto:camera" },
-    { value: "folder", icon: "fluent-emoji:file-folder" },
-  ];
-
   return (
     <ToggleButtonGroup value={selected} exclusive onChange={handleChange} sx={{ display: "flex", gap: 1 }}>
-      {iconOptions.map(({ value, icon }) => (
+      {CommonIcons.map(({ value, icon }) => (
         <ToggleButton
           key={value}
-          value={value}
+          value={icon}
           disableRipple
           disableFocusRipple
           sx={{
