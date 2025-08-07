73897c294aede67eca47e044ed947c627a762720
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:39:51 -0300

    Feat: Modificação da RulePage para utilizar o novo gerenciador de view das regras

--- src/renderer/src/views/RulePage/index.tsx
+++ src/renderer/src/views/RulePage/index.tsx
@@ -1,34 +1,7 @@
-import { useEffect } from "react";
-import { useRuleStore } from "../../store/ruleStore";
-import RuleTabs from "../../components/RuleTabs";
-import ContentWrapper from "../../components/ContentWrapper";
-import RulePopup from "../../components/RulePopup";
-import { useRulePopupStore } from "../../store/popupRuleStore";
+import RuleManagementView from "../RuleManagementView";
 
 const RulePage = () => {
-  const { rules, fetchRules } = useRuleStore();
-  const { closePopup, openPopup, isOpen } = useRulePopupStore();
-
-  useEffect(() => {
-    fetchRules();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (rules.length === 0) return;
-
-  return (
-    <ContentWrapper
-      title="Regras de Organização"
-      commonBtn={{
-        style: "contained",
-        Action: () => openPopup("create"),
-        text: "Adicionar Regra",
-      }}
-    >
-      <RuleTabs tabContents={rules} />
-      <RulePopup isOpen={isOpen} onClose={() => closePopup()}  />
-    </ContentWrapper>
-  );
+  return <RuleManagementView mode="page" />;
 };
 
 export default RulePage;
