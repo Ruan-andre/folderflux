ab12902372494b0481cedd4c1bf89e52f733b03e
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:53:53 -0300

    Feat: Adição de handler para alternar o status das configurações do sistema

--- src/main/handlers/domain/settings.ts
+++ src/main/handlers/domain/settings.ts
@@ -1,8 +1,11 @@
 import { ipcMain } from "electron";
-import { getSettings } from "../../services/settingsService";
+import { getSettings, toggleSettingActive } from "../../services/settingsService";
 
 export function registerSettingsHandlers() {
   ipcMain.handle("get-settings", async () => {
     return await getSettings();
+  });
+  ipcMain.handle("toggle-setting-active", async (_e, id: number) => {
+    return await toggleSettingActive(id);
   });
 }
