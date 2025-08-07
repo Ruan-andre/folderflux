8913a99caabb05db5872c9c1d7418ad1989b0bb9
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:40:58 -0300

    Feat: :sparkles: Implementação da página de perfis

--- src/renderer/src/views/ProfilePage/index.tsx
+++ src/renderer/src/views/ProfilePage/index.tsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import ContentWrapper from "../../components/ContentWrapper";
 import Profile from "../../components/Profile";
 import ProfilePopup from "../../components/ProfilePopup";
+import { useProfileStore } from "../../store/profileStore";
+import { useProfilePopupStore } from "../../store/popupProfileStore";
+import { Box } from "@mui/material";
 
 const ProfilePage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { profiles, getProfiles } = useProfileStore();
+  const { openPopup } = useProfilePopupStore();
+
+  const handleUpdateSuccess = async () => {
+    await getProfiles();
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      await getProfiles();
+    }
+    fetchData();
+  }, [getProfiles]);
 
   return (
     <ContentWrapper
       title="Perfis de Organização"
-      action="btn"
-      btn={{ Action: () => setIsOpen(true), text: "Criar Perfil" }}
+      commonBtn={{ Action: () => openPopup("create"), text: "Criar Perfil" }}
     >
-      <div style={{ display: "flex", gap: "2rem", alignItems: "center", flexWrap: "wrap" }}>
-        <Profile />
-      </div>
-      <ProfilePopup isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <Box display={"flex"} gap={"3rem 3rem"} flexWrap="wrap" justifyContent={"center"}>
+        {profiles.map((p) => {
+          return (
+            <div
+              key={p.id}
+              style={{ display: "flex", gap: "2rem", alignItems: "center", flexWrap: "wrap" }}
+              onClick={() => {
+                openPopup("edit", p);
+              }}
+            >
+              <Profile
+                key={p.id}
+                id={p.id}
+                name={p.name}
+                description={p.description}
+                rules={p.rules}
+                folders={p.folders}
+                iconId={p.iconId}
+                isActive={p.isActive}
+              />
+            </div>
+          );
+        })}
+      </Box>
+
+      <ProfilePopup onUpdateSuccess={handleUpdateSuccess} />
     </ContentWrapper>
   );
 };
