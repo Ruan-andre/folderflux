642f74b37ad8a2572d2b13a9bee19e78ed61eb7e
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:39:19 -0300

    Feat: :sparkles: Implementação da página de configurações do sistema

--- src/renderer/src/views/SettingPage/index.tsx
+++ src/renderer/src/views/SettingPage/index.tsx
@@ -1,38 +1,71 @@
 import ContentWrapper from "../../components/ContentWrapper";
 import GenericListItems from "../../components/GenericListItems";
+import { useEffect, useState, useMemo } from "react";
+import { SettingsSchema } from "~/src/db/schema";
+import { useSnackbar } from "../../context/SnackBarContext";
 import GenericListItemsType from "../../types/GenericListItemsType";
-import CustomSwitch from "../../components/CustomSwitch";
-const listaConfigGeralMock: GenericListItemsType[] = [
-  {
-    title: "Iniciar com o sistema",
-    subtitle: "Executar o TidyFlux quando computador for ligado",
-    iconsAction: [<CustomSwitch />],
-  },
-  {
-    title: "Monitoramento em tempo real",
-    subtitle: "Organizar arquivos automaticamente quando detectar mudanças",
-    iconsAction: [<CustomSwitch />],
-  },
-  {
-    title: "Notificações",
-    subtitle: "Exibir notificações quando arquivos forem organizados",
-    iconsAction: [<CustomSwitch />],
-  },
-];
 
-const listaConfigAparenciaMock: GenericListItemsType[] = [
-  {
-    title: "Tema escuro",
-    subtitle: "Se ativado, o sistema ficará no modo escuro, do contrário, ficará com o tema claro.",
-    iconsAction: [<CustomSwitch checked />],
-  },
-];
 const SettingPage = () => {
+  const { showMessage } = useSnackbar();
+
+  const [settings, setSettings] = useState<SettingsSchema[]>([]);
+
+  const generalSettingsList: GenericListItemsType[] = useMemo(() => {
+    return settings
+      .filter((s) => s.category === "general")
+      .map((s) => ({
+        id: s.id,
+        title: s.title,
+        subtitle: s.description ?? "",
+        active: s.isActive,
+      }));
+  }, [settings]);
+
+  const appearanceSettingsList: GenericListItemsType[] = useMemo(() => {
+    return settings
+      .filter((s) => s.category === "appearance")
+      .map((s) => ({
+        id: s.id,
+        title: s.title,
+        subtitle: s.description ?? "",
+        active: s.isActive,
+      }));
+  }, [settings]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await window.api.settings.getSettings();
+      setSettings(response);
+    }
+    fetchData();
+  }, []);
+
+  const handleToggle = async (id: number) => {
+    const originalSettings = [...settings];
+    setSettings((currentSettings) =>
+      currentSettings.map((setting) =>
+        setting.id === id ? { ...setting, isActive: !setting.isActive } : setting
+      )
+    );
+
+    try {
+      await window.api.settings.toggleSettingActive(id);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      showMessage("Erro ao atualizar a configuração.", "error");
+      setSettings(originalSettings);
+    }
+  };
+
+  if (settings.length === 0) return null;
+
   return (
     <ContentWrapper title="Configurações">
       <ContentWrapper title="Geral" hr boxShadow="none" titleSize={20} gap="1rem" padding="1rem">
         <GenericListItems
-          list={listaConfigGeralMock}
+          list={generalSettingsList}
+          btnSwitch
+          onToggle={handleToggle}
           isButton={false}
           borderBottom="1px solid rgba(245, 245, 245, 0.27)"
           listItemPadding="1.5rem"
@@ -41,7 +74,9 @@ const SettingPage = () => {
 
       <ContentWrapper title="Aparência" hr boxShadow="none" titleSize={20} gap="1rem" padding="1rem">
         <GenericListItems
-          list={listaConfigAparenciaMock}
+          list={appearanceSettingsList}
+          btnSwitch
+          onToggle={handleToggle}
           isButton={false}
           borderBottom="1px solid rgba(245, 245, 245, 0.27)"
           listItemPadding="1.5rem"
