cdd3276f043c9d867386f0213ef9bfa4611d9bbc
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:53:21 -0300

    Fix: Corrigindo abertura do dialog do sistema operacional para impedir interação com a janela principal do programa

--- src/main/handlers/system/dialog.ts
+++ src/main/handlers/system/dialog.ts
@@ -1,21 +1,27 @@
-import { ipcMain, dialog } from "electron";
+import { ipcMain, dialog, BrowserWindow } from "electron";
 
 export function registerDialogHandlers() {
-  ipcMain.handle("select-directory", async () => {
-    const result = await dialog.showOpenDialog({
-      properties: ["openDirectory"],
-    });
+  ipcMain.handle("select-directory", async (event) => {
+    const win = BrowserWindow.fromWebContents(event.sender);
+    if (win) {
+      const result = await dialog.showOpenDialog(win, {
+        properties: ["openDirectory"],
+      });
 
-    if (result.canceled || result.filePaths.length === 0) return null;
-    return result.filePaths[0];
+      if (result.canceled || result.filePaths.length === 0) return null;
+      return result.filePaths[0];
+    }
   });
 
-  ipcMain.handle("select-multiple-directories", async () => {
-    const result = await dialog.showOpenDialog({
-      properties: ["openDirectory", "multiSelections"],
-    });
+  ipcMain.handle("select-multiple-directories", async (event) => {
+    const win = BrowserWindow.fromWebContents(event.sender);
+    if (win) {
+      const result = await dialog.showOpenDialog(win, {
+        properties: ["openDirectory", "multiSelections"],
+      });
 
-    if (result.canceled || result.filePaths.length === 0) return null;
-    return result.filePaths;
+      if (result.canceled || result.filePaths.length === 0) return null;
+      return result.filePaths;
+    }
   });
 }
