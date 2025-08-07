5d77da8759dc33796bdd9ad337c76a25f88f3113
Andre Ruan <andreruan80@gmail.com>
2025-08-06 21:52:42 -0300

    Feat: Adição de handlers para serviços dos arquivos utilizando fs

---
+++ src/main/handlers/system/file.ts
@@ -0,0 +1,22 @@
+import { ipcMain } from "electron";
+import { getFilesInfo, getStats, isDirectory, moveFile } from "../../services/fileService";
+
+export function registerFileHandlers() {
+  ipcMain.handle("fs:get-files-info", async (_e, path: string) => {
+    return await getFilesInfo(path);
+  });
+
+  ipcMain.handle("fs:move-file", async (_e, path: string, newPath: string) => {
+    return await moveFile(path, newPath);
+  });
+
+  ipcMain.handle("fs:is-directory", async (event, path) => {
+    return await isDirectory(path);
+  });
+
+  ipcMain.handle("fs:get-stats", async (event, paths: string[]) => {
+    return await getStats(paths);
+  });
+
+  
+}
