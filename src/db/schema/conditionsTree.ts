7f31374bb8fa55e26028efc275acc73aad703833
Andre Ruan <andreruan80@gmail.com>
2025-08-08 19:57:58 -0300

    Feat: Adição de novo schema para unificar os schemas conditionGroups e conditions que foram deletados

---
+++ src/db/schema/conditionsTree.ts
@@ -0,0 +1,54 @@
+import { relations } from "drizzle-orm";
+import { sqliteTable, text, integer, type AnySQLiteColumn } from "drizzle-orm/sqlite-core";
+import { RuleTable } from "./rules";
+
+export const ConditionsTreeTable = sqliteTable("conditions_tree", {
+  id: integer("id").primaryKey(),
+  type: text("type", { enum: ["group", "condition"] }).notNull(),
+  displayOrder: integer("display_order").notNull().default(0),
+  ruleId: integer("rule_id")
+    .references(() => RuleTable.id, { onDelete: "cascade" })
+    .notNull(),
+  parentGroupId: integer("parent_group_id").references((): AnySQLiteColumn => ConditionsTreeTable.id, {
+    onDelete: "cascade",
+  }),
+  // Colunas específicas de GRUPO (podem ser nulas)
+  operator: text("operator", { enum: ["AND", "OR"] }),
+  // Colunas específicas de CONDIÇÃO (podem ser nulas)
+  field: text("field", {
+    enum: ["fileName", "fileExtension", "creationDate", "modifiedDate", "fileSize"],
+  }),
+  fieldOperator: text("field_operator", {
+    enum: [
+      "contains",
+      "notContains",
+      "isBetween",
+      "startsWith",
+      "endsWith",
+      "equals",
+      "lessThan",
+      "higherThan",
+    ],
+  }),
+  value: text("value"),
+  value2: text("value2"),
+});
+
+// Relações para a árvore
+export const conditionsTreeRelations = relations(ConditionsTreeTable, ({ one, many }) => ({
+  rule: one(RuleTable, {
+    fields: [ConditionsTreeTable.ruleId],
+    references: [RuleTable.id],
+  }),
+  parentGroup: one(ConditionsTreeTable, {
+    fields: [ConditionsTreeTable.parentGroupId],
+    references: [ConditionsTreeTable.id],
+    relationName: "children", // O pai tem a relação com os filhos
+  }),
+  children: many(ConditionsTreeTable, {
+    relationName: "children", // Os filhos se relacionam com o pai
+  }),
+}));
+
+export type ConditionTreeSchema = typeof ConditionsTreeTable.$inferSelect;
+export type NewConditionTree = typeof ConditionsTreeTable.$inferInsert;
