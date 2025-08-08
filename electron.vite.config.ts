150f7bda70b02434ab81299475fa1d4319438915
Andre Ruan <andreruan80@gmail.com>
2025-08-08 20:08:21 -0300

    Fix: Correção da configuração dos paths utilizando "~" e "@"

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,35 +3,42 @@ import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
+const rootDir = __dirname;
+
 export default defineConfig({
   main: {
+    resolve: {
+      alias: {
+        "~": resolve(rootDir, "src"),
+        "@": resolve(rootDir, "src"),
+      },
+    },
     plugins: [
       externalizeDepsPlugin(),
       viteStaticCopy({
         targets: [
-          {
-            src: "resources/*",
-            dest: "resources",
-          },
-          {
-            src: "src/db/**/*",
-            dest: "db/",
-          },
-          {
-            src: "src/shared/**/*",
-            dest: "shared/",
-          },
+          { src: "resources/*", dest: "resources" },
+          { src: "src/db/**/*", dest: "db/" },
+          { src: "src/shared/**/*", dest: "shared/" },
         ],
       }),
     ],
   },
   preload: {
+    resolve: {
+      alias: {
+        "~": resolve(rootDir, "src"),
+        "@": resolve(rootDir, "src"),
+      },
+    },
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     resolve: {
       alias: {
-        "@renderer": resolve("src/renderer/src"),
+        "~": resolve(rootDir, "src"),
+        "@": resolve(rootDir, "src"),
+        "@renderer": resolve(rootDir, "src/renderer/src"),
       },
     },
     plugins: [react()],
