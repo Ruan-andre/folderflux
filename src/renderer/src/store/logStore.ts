217be2e09984876f65e448108f098cba6581bb4f
Andre Ruan <andreruan80@gmail.com>
2025-08-19 19:44:29 -0300

    Feat: Adição de store para genreciamento do estado dos logs com zustand

---
+++ src/renderer/src/store/logStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { DbResponse } from "../../../shared/types/DbResponse";
+import { LogMetadata } from "~/src/shared/types/LogMetaDataType";
+
+type LogState = {
+  logs: LogMetadata[];
+  getLogs: (lastId?: number) => Promise<number | undefined>;
+  deleteLog: (id: number) => Promise<DbResponse<void>>;
+  deleteAllLogs: () => Promise<DbResponse<void>>;
+};
+
+export const useLogStore = create<LogState>((set) => ({
+  logs: [],
+  getLogs: async (lastId?: number) => {
+    const response = await window.api.organization.getLogs(lastId);
+    if (response.status && response.items) {
+      if (lastId) {
+        set((state) => ({
+          logs: [...state.logs, ...(response.items ?? [])],
+        }));
+      } else {
+        set({ logs: response.items });
+      }
+    }
+    return response.items?.length;
+  },
+  deleteLog: async (id: number) => {
+    const response = await window.api.organization.deleteLogById(id);
+    if (response.status) {
+      set((state) => ({ logs: state.logs.filter((log) => log.id !== id) }));
+    }
+    return response;
+  },
+  deleteAllLogs: async () => {
+    const response = await window.api.organization.deleteAllLogs();
+    if (response.status) {
+      set({ logs: [] });
+    }
+    return response;
+  },
+}));
