c8dfe9bdf88c59edcc2b2bcb7cf0adbcb7b2ea89
Andre Ruan <andreruan80@gmail.com>
2025-08-19 19:41:21 -0300

    Feat: :tada: Implementação do campo de logs

--- src/renderer/src/views/HomePage/index.tsx
+++ src/renderer/src/views/HomePage/index.tsx
@@ -1,85 +1,195 @@
-import React, { useState } from "react";
-import { Modal } from "@mui/material";
+import { useEffect, useState, useRef, RefObject } from "react";
+import { Modal, Typography } from "@mui/material";
 import ContentWrapper from "../../components/ContentWrapper";
 import FolderDropZone from "../../components/FolderDropZone";
-import GenericCard from "../../components/GenericCard";
 import GenericListItems from "../../components/GenericListItems";
-import Icon from "../../assets/icons/index";
 import RuleManagementView from "../RuleManagementView";
 import { useConfirmDialog } from "../../context/ConfirmDialogContext";
 import { FullRule } from "~/src/shared/types/RuleWithDetails";
+import GenericListItemsType from "../../types/GenericListItemsType";
+import CommonIcons from "../../types/CommonIconsType";
+import { useSnackbar } from "../../context/SnackBarContext";
+import { DbResponse } from "~/src/shared/types/DbResponse";
+import OrganizationLogPopup from "../../components/OrganizationLogPopup";
+import { AlertColor } from "@mui/material";
+import GenericCard from "../../components/GenericCard";
+import Icon from "../../assets/icons";
+import { LogMetadata } from "~/src/shared/types/LogMetaDataType";
+import { useIntersectionObserver } from "../../hooks/intersectionObserverHook"; // ✅ IMPORTADO
 import { PathStats } from "~/src/shared/types/pathStatsType";
+import { useLogStore } from "../../store/logStore";
 
 const HomePage = () => {
   const { showConfirm } = useConfirmDialog();
+  const { showMessage } = useSnackbar();
+
   const [isRuleSelectorOpen, setIsRuleSelectorOpen] = useState<boolean>(false);
-  const [selectedFolders, setSelectedFolders] = useState<string[]>([]);
+  const [foldersForQuickClean, setFoldersForQuickClean] = useState<string[]>([]);
+  const [lastLogId, setLastLogId] = useState<number | undefined>(undefined);
+  const lastLogRef = useRef<HTMLLIElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const [selectedLog, setSelectedLog] = useState<LogMetadata | null>(null);
+  const { logs, deleteAllLogs, getLogs, deleteLog } = useLogStore();
 
-  const handleItemsDropped = async (paths: string[], callback?: () => void) => {
-    if (!paths || paths.length === 0) return;
-
-    const stats = await window.api.getStatsForPaths(paths);
-    const folders = stats.filter((s) => s.isDirectory);
-
-    if (folders.length === 0) {
-      if (callback) callback();
-      return;
-    }
-    const folderPaths = folders.map((f) => f.path);
-    showConfirm(
-      {
-        message: await montaMensagemPastas(folders),
-        confirmText: "Perfil Padrão",
-        thirdButton: { text: "Selecionar Regras" },
-        confirmBtnColor: "success",
-      },
-      async () => {
-        await window.api.organization.defaultOrganization(folderPaths);
-      },
-      async () => {
-        setSelectedFolders(folderPaths);
-        setIsRuleSelectorOpen(true);
+  useEffect(() => {
+    async function fetchData() {
+      if (isLoading || !hasMore) return;
+      setIsLoading(true);
+      try {
+        const newLogsCount = await getLogs(lastLogId);
+        if (newLogsCount && newLogsCount < 10) {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
       }
-    );
+    }
+
+    fetchData();
+    console.count("vezes");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getLogs, lastLogId]);
+
+  const recentActivityList: GenericListItemsType[] = logs.map((item) => {
+    const icon =
+      item.type === "organization"
+        ? CommonIcons.find((x) => x.value === "folder")
+        : item.type === "cleanup"
+          ? CommonIcons.find((x) => x.value === "clean")
+          : CommonIcons.find((x) => x.value === "folder-error");
+    return {
+      id: item.id,
+      title: item.title,
+      subtitle: item.description,
+      icon: icon?.icon,
+      dateItem: item.createdAt,
+    };
+  });
+
+  useIntersectionObserver(lastLogRef as RefObject<Element>, () => {
+    if (isLoading || !hasMore || logs.length === 0) return;
+    const nextId = logs[logs.length - 1].id;
+    setLastLogId(nextId);
+  });
+
+  const handleOpenDetails = (logId: number) => {
+    const logToShow = logs.find((log) => log.id === logId);
+    if (logToShow) {
+      let logMetadata: LogMetadata;
+      const { type } = logToShow;
+      switch (type) {
+        case "organization":
+          logMetadata = {
+            ...logToShow,
+            type,
+          };
+          break;
+        case "cleanup":
+          logMetadata = {
+            ...logToShow,
+            type,
+          };
+          break;
+        case "error":
+          logMetadata = {
+            ...logToShow,
+            type,
+          };
+          break;
+        default:
+          throw new Error(`Tipo de log desconhecido: ${type}`);
+      }
+      setSelectedLog(logMetadata);
+      setIsDetailsOpen(true);
+    }
+  };
+
+  const handleItemsDropped = async (paths: string[], handleError?: () => void) => {
+    if (!paths || paths.length === 0) return;
+    const stats = await window.api.getStatsForPaths(paths);
+    const folders = stats.filter((s) => s.isDirectory).map((f) => f.path);
+    if (folders.length > 0) showQuickCleanOptions(folders);
+    else if (handleError) handleError();
   };
 
   const handleClickSelectFolder = async () => {
-    const response = await window.api.dialog.selectMultipleDirectories();
-    if (response && response.length > 0) {
-      showConfirm(
-        {
-          message: await montaMensagemPastas(response),
-          confirmText: "Perfil Padrão",
-          thirdButton: { text: "Selecionar Regras" },
-          confirmBtnColor: "success",
-        },
-        async () => {
-          if (response) await window.api.organization.defaultOrganization(response);
-        },
-        async () => {
-          setSelectedFolders(response);
-          setIsRuleSelectorOpen(true);
-        }
-      );
-    }
+    const paths = await window.api.dialog.selectMultipleDirectories();
+    if (paths && paths.length > 0) showQuickCleanOptions(paths);
   };
 
   const handleRuleSelectionSave = async (selectedRules: FullRule[]) => {
-    if (selectedFolders && selectedRules.length > 0) {
-      await window.api.organization.organizeWithSelectedRules(selectedRules, selectedFolders);
-    }
-
     setIsRuleSelectorOpen(false);
+    if (foldersForQuickClean.length > 0 && selectedRules.length > 0) {
+      const response = await window.api.organization.organizeWithSelectedRules(
+        selectedRules,
+        foldersForQuickClean
+      );
+
+      handleMessage(response);
+      if (response.items && response.items > 0) {
+        setLastLogId(undefined);
+        getLogs();
+      }
+    }
   };
 
   async function montaMensagemPastas(folders: PathStats[] | string[]) {
     let folderNames = "";
-    let stats: PathStats[] = [];
+    let stats: PathStats[] = folders as PathStats[];
     if (typeof folders[0] === "string") stats = await window.api.getStatsForPaths(folders as string[]);
     folderNames = stats.map((f, index) => `\n${++index} ${f.name}`).join("");
 
     return `Organizar ${folders.length} pasta(s):\n${folderNames}`;
   }
+  async function showQuickCleanOptions(folderPaths: string[]) {
+    const message = await montaMensagemPastas(folderPaths);
+    showConfirm(
+      {
+        message,
+        confirmText: "Usar Perfil Padrão",
+        thirdButton: { text: "Selecionar Regras" },
+        confirmBtnColor: "success",
+      },
+      async () => {
+        const response = await window.api.organization.defaultOrganization(folderPaths);
+        if (response.items && response.items > 0) {
+          getLogs();
+        }
+        handleMessage(response);
+      },
+      async () => {
+        // Ação para "Selecionar Regras"
+        setFoldersForQuickClean(folderPaths);
+        setIsRuleSelectorOpen(true);
+      }
+    );
+  }
+
+  function handleMessage(response: DbResponse<number>) {
+    let messageType: AlertColor = response.status ? "success" : "error";
+    if (response.status && response.items === 0) messageType = "info";
+    showMessage(response.message, messageType);
+  }
+
+  const handleDeleteAllLogs = async () => {
+    showConfirm({}, async () => {
+      const response = await deleteAllLogs();
+      const messageType = response.status ? "success" : "error";
+      showMessage(response.message, messageType);
+      setLastLogId(undefined);
+    });
+  };
+
+  const handleDeleteLogById = async (logId: number) => {
+    const response = await deleteLog(logId);
+    const messageType = response.status ? "success" : "error";
+    showMessage(response.message, messageType);
+  };
+
   return (
     <ContentWrapper minHeightStyle="95vh" justifyContent="flex-start">
       <div className="flex-center">
@@ -91,6 +201,7 @@ const HomePage = () => {
           widthCard="35rem"
           heightCard="11rem"
         />
+
         <GenericCard
           title="Status dos Perfis"
           subtitle="3 perfis Ativos, 1 Inativo"
@@ -99,6 +210,7 @@ const HomePage = () => {
           widthCard="35rem"
           heightCard="11rem"
         />
+
         <GenericCard
           title="Como usar?"
           subtitle="Guia rápido de início"
@@ -108,12 +220,40 @@ const HomePage = () => {
           heightCard="11rem"
         />
       </div>
-
       <FolderDropZone onClick={handleClickSelectFolder} onItemsDropped={handleItemsDropped} />
-
-      <ContentWrapper title="Atividade Recente" hr>
-        <GenericListItems list={[]} />
+      <ContentWrapper
+        title="Atividade Recente"
+        maxHeightStyle={"52rem"}
+        commonBtn={
+          recentActivityList.length > 0
+            ? {
+                text: "Apagar todos",
+                style: "outlined",
+                color: "error",
+                Action: () => handleDeleteAllLogs(),
+              }
+            : undefined
+        }
+        hr
+      >
+        {recentActivityList.length > 0 ? (
+          <>
+            <GenericListItems
+              isButton
+              btnDelete
+              onClickDelete={handleDeleteLogById}
+              onClickListItem={handleOpenDetails}
+              lastItemRef={lastLogRef as RefObject<HTMLLIElement>}
+              list={recentActivityList}
+            />
+          </>
+        ) : (
+          <Typography fontSize={"1.4rem"} textAlign={"center"}>
+            Nenhum registro
+          </Typography>
+        )}
       </ContentWrapper>
+
       <Modal
         open={isRuleSelectorOpen}
         onClose={() => setIsRuleSelectorOpen(false)}
@@ -126,6 +266,12 @@ const HomePage = () => {
           onCancel={() => setIsRuleSelectorOpen(false)}
         />
       </Modal>
+      <OrganizationLogPopup
+        key={selectedLog?.id}
+        log={selectedLog}
+        isOpen={isDetailsOpen}
+        onClose={() => setIsDetailsOpen(false)}
+      />
     </ContentWrapper>
   );
 };
