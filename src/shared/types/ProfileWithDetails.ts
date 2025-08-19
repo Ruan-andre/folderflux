7e6e73f16ebd30cd0a391414abf703d094dd084d
Andre Ruan <andreruan80@gmail.com>
2025-08-19 19:33:57 -0300

    Fix: Correção do tipo das rules vinculadas aos perfis.

--- src/shared/types/ProfileWithDetails.ts
+++ src/shared/types/ProfileWithDetails.ts
@@ -1,9 +1,9 @@
-import { FolderSchema, NewFolder, NewProfile, NewRule, ProfileSchema, RuleSchema } from "~/src/db/schema";
+import { FolderSchema, NewFolder, NewProfile, NewRule, ProfileSchema } from "~/src/db/schema";
 import { FullRule } from "./RuleWithDetails";
 
 export type FullProfile = (ProfileSchema | NewProfile) & {
   folders: FolderSchema[];
-  rules: (RuleSchema | FullRule)[];
+  rules: FullRule[];
 };
 
 export type NewFullProfile = NewProfile & {
