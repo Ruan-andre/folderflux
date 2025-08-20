3f6c4095420b14d9889f7dcd2be158cd65a1ef45
Andre Ruan <andreruan80@gmail.com>
2025-08-20 18:50:11 -0300

    Feat: Validação de edição das pastas

--- src/renderer/src/components/FolderPopup/index.tsx
+++ src/renderer/src/components/FolderPopup/index.tsx
@@ -4,19 +4,26 @@ import GenericInput from "../GenericInput";
 import { useEffect, useState } from "react";
 import { FolderSchema } from "~/src/db/schema";
 import { useFolderPopupStore } from "../../store/popupFolderStore";
+import { useSnackbar } from "../../context/SnackBarContext";
 
 const FolderPopup = ({ onUpdateSuccess }: { onUpdateSuccess: () => void }) => {
   const theme = useTheme();
   const { isOpen, folderToEdit, closePopup } = useFolderPopupStore();
   const [name, setName] = useState("");
   const [fullPath, setFullPath] = useState("");
+  const { showMessage } = useSnackbar();
 
   const handleSubmit = async () => {
     if (folderToEdit) {
       const updatedFolder: FolderSchema = { ...folderToEdit, name, fullPath };
-      await window.api.folder.updateFolder(updatedFolder);
-      onUpdateSuccess();
-      closePopup();
+      const isDirectory = (await window.api.getStatsForPaths([fullPath]))[0]?.isDirectory;
+      if (isDirectory === true) {
+        await window.api.folder.updateFolder(updatedFolder);
+        onUpdateSuccess();
+        closePopup();
+      } else {
+        showMessage("Não é possível inserir diretório de arquivos, ou diretórios inexistentes.", "error");
+      }
     }
   };
 
