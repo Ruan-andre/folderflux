5e2a9404f70109741fe1898afd61b2a84fdac10f
Andre Ruan <andreruan80@gmail.com>
2025-08-21 19:21:03 -0300

    Refactor: Adiciona nova propriedade nos objetos das seeds

--- src/db/seeds/settingsSeed.ts
+++ src/db/seeds/settingsSeed.ts
@@ -6,23 +6,27 @@ export const settingSeed: NewSettings[] = [
     description: "Executar o TidyFlux quando computador for ligado",
     category: "general",
     isActive: true,
+    type: "startWithOS",
   },
   {
     title: "Monitoramento em tempo real",
     description: "Organizar arquivos automaticamente quando detectar mudanças",
     category: "general",
     isActive: true,
+    type: "realTime",
   },
   {
     title: "Notificações",
     description: "Exibir notificações quando arquivos forem organizados",
     category: "general",
     isActive: true,
+    type: "notifications",
   },
   {
     title: "Tema escuro",
     description: "Se ativado, o sistema ficará no modo escuro, do contrário, ficará com o tema claro.",
     category: "appearance",
     isActive: true,
+    type: "darkMode",
   },
 ];
