1613c451780c6b1c93baa75dac24c0a8f451276a
Andre Ruan <andreruan80@gmail.com>
2025-08-21 19:19:33 -0300

    Refactor: Adiciona retorno nas funções de organização rápida para exibição de mensagem no frontend

--- src/main/core/quickOrganization.ts
+++ src/main/core/quickOrganization.ts
@@ -1,15 +1,16 @@
 import { FullRule } from "~/src/shared/types/RuleWithDetails";
 import { getSystemProfile } from "../services/profileService";
 import RuleEngine from "./ruleEngine";
+// import RuleEngine from "./ruleEngine";
 
 export async function defaultOrganization(paths: string[]) {
   const defaultProfile = await getSystemProfile();
   if (defaultProfile) {
     const rules: FullRule[] = defaultProfile.rules.filter((r) => r.isActive) as FullRule[];
-    await RuleEngine.process(rules, paths);
-  }
+    return RuleEngine.process(rules, paths, defaultProfile.name);
+  } else throw "Perfil padrão não encontrado";
 }
 
 export async function organizeWithSelectedRules(rules: FullRule[], paths: string[]) {
-  await RuleEngine.process(rules, paths);
+  return await RuleEngine.process(rules, paths);
 }
