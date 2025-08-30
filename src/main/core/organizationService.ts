0a771508647af887ca801c24d2ce1e1cf3e48bff
Andre Ruan <andreruan80@gmail.com>
2025-08-30 15:18:27 -0300

    Refactor: Refatoração de services para receber o DbContext via parâmetro e não importar mais diretamente (injeção de dependência)

--- src/main/core/organizationService.ts
+++ src/main/core/organizationService.ts
@@ -1,32 +1,44 @@
 import { FullRule } from "~/src/shared/types/RuleWithDetails";
 import { getSystemProfile } from "../services/profileService";
 import RuleEngine from "./ruleEngine";
-import { db } from "../../db";
+import { DbOrTx } from "../../db";
 import { FullProfile } from "~/src/shared/types/ProfileWithDetails";
+import { LogMetadata } from "~/src/shared/types/LogMetaDataType";
 
-export async function defaultOrganization(paths: string[]) {
+export async function defaultOrganization(
+  db: DbOrTx,
+  paths: string[],
+  onLogAdded?: (logs: LogMetadata | LogMetadata[]) => void
+) {
   const defaultProfile = await getSystemProfile(db);
   if (defaultProfile) {
     const rules: FullRule[] = defaultProfile.rules.filter((r) => r.isActive) as FullRule[];
-    return paths.length > 0 ? RuleEngine.process(db, rules, paths, defaultProfile.name) : 0;
+    return RuleEngine.process(db, rules, paths, defaultProfile.name, onLogAdded);
   } else throw "Perfil padrão não encontrado";
 }
 
-export async function organizeWithSelectedRules(rules: FullRule[], paths: string[]) {
-  if (rules.length > 0 && paths.length > 0) {
-    return await RuleEngine.process(db, rules, paths);
-  }
-  return 0;
+export async function organizeWithSelectedRules(
+  db: DbOrTx,
+  rules: FullRule[],
+  paths: string[],
+  onLogAdded?: (logs: LogMetadata | LogMetadata[]) => void
+) {
+  return await RuleEngine.process(db, rules, paths, undefined, onLogAdded);
 }
 
-export async function organizeWithSelectedProfiles(profiles: FullProfile[], paths: string[]) {
-  if (profiles.length > 0 && paths.length > 0) {
-    let response = 0;
-    for (const profile of profiles) {
-      const rules = profile.rules.filter((r) => r.isActive);
-      response += (await RuleEngine.process(db, rules, paths, profile.name))?.items ?? 0;
+export async function organizeWithSelectedProfiles(
+  db: DbOrTx,
+  profiles: FullProfile[],
+  paths: string[],
+  onLogAdded?: (logs: LogMetadata | LogMetadata[]) => void
+) {
+  let response;
+  for (const profile of profiles) {
+    const rules = profile.rules.filter((r) => r.isActive);
+    const responseOrganization = await RuleEngine.process(db, rules, paths, profile.name, onLogAdded);
+    if (responseOrganization.items && responseOrganization.items > 0 && !response) {
+      response = responseOrganization;
     }
-    return response;
   }
-  return 0;
+  return response;
 }
