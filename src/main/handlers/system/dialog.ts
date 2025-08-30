3a07e9f493c01814947b7ca7ebd6dd5d05e3a288
Andre Ruan <andreruan80@gmail.com>
2025-08-30 15:24:09 -0300

    Feat: Adição de propriedade para já abrir o seletor de pastas do sistema operacional em diretório específico para o tutorial

--- src/main/handlers/system/dialog.ts
+++ src/main/handlers/system/dialog.ts
@@ -1,4 +1,5 @@
-import { ipcMain, dialog, BrowserWindow } from "electron";
+import { ipcMain, dialog, BrowserWindow, app } from "electron";
+import path from "path";
 
 export function registerDialogHandlers() {
   ipcMain.handle("select-directory", async (event) => {
@@ -13,11 +14,21 @@ export function registerDialogHandlers() {
     }
   });
 
-  ipcMain.handle("select-multiple-directories", async (event) => {
+  ipcMain.handle("select-multiple-directories", async (event, isTutorialPath) => {
     const win = BrowserWindow.fromWebContents(event.sender);
     if (win) {
+      let tutorialDir;
+      if (isTutorialPath) {
+        const resourcesPath = app.isPackaged
+          ? process.resourcesPath // Em produção, aponta para a pasta 'resources'
+          : app.getAppPath(); // Em desenvolvimento, aponta para a raiz do projeto
+
+        tutorialDir = path.join(resourcesPath, "resources", "tutorial-example");
+      }
+
       const result = await dialog.showOpenDialog(win, {
         properties: ["openDirectory", "multiSelections"],
+        defaultPath: tutorialDir,
       });
 
       if (result.canceled || result.filePaths.length === 0) return null;
