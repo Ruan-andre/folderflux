d238ab3539cac83c333a11652739999e343fc966
Andre Ruan <andreruan80@gmail.com>
2025-09-07 19:59:04 -0300

    Feat: Personalização da interface StepOptions do Shepherdjs

---
+++ src/renderer/src/types/shepherd.d.ts
@@ -0,0 +1,21 @@
+// Em: src/types/shepherd.d.ts
+
+import "shepherd.js";
+import { Root } from "react-dom/client";
+
+// Estende os tipos do módulo 'shepherd.js'
+declare module "shepherd.js" {
+  interface Tour {
+    _startAdvancedTour?: () => void;
+  }
+
+  interface StepOptions {
+    page?: string;
+    advanceOn?: {
+      selector: string;
+      event: keyof HTMLElementEventMap;
+    };
+    // Guarda a referência da raiz do React para limpeza (unmount)
+    _reactRoot?: Root;
+  }
+}
