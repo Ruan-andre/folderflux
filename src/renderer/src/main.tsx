d146a1a29c541a5bbd22d14ade91c4a10238b111
Andre Ruan <andreruan80@gmail.com>
2025-09-07 20:02:35 -0300

    Refactor: Condição para renderizar as duas janelas principais do programa

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Componentes principais
 import App from "./App";
+
+// Seus imports de CSS e Providers
 import "./assets/css/globals.css";
 import "shepherd.js/dist/css/shepherd.css";
 import "./assets/css/tour.css";
@@ -8,16 +12,30 @@ import { SnackbarProvider } from "./context/SnackBarContext";
 import { ConfirmDialogProvider } from "./context/ConfirmDialogContext";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "./context/ThemeContext";
+import AudioPlayer from "./components/audioPlayer/main";
+import { TourController } from "./components/TourController";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <CssBaseline />
-      <ConfirmDialogProvider>
-        <SnackbarProvider>
-          <App />
-        </SnackbarProvider>
-      </ConfirmDialogProvider>
-    </ThemeProvider>
-  </StrictMode>
-);
+const container = document.getElementById("root");
+
+const root = createRoot(container!);
+
+const urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.get("view") === "audioPlayer") {
+  root.render(<AudioPlayer />);
+} else {
+
+  root.render(
+    <StrictMode>
+      <ThemeProvider>
+        <CssBaseline />
+        <ConfirmDialogProvider>
+          <SnackbarProvider>
+            <App />
+            <TourController />
+          </SnackbarProvider>
+        </ConfirmDialogProvider>
+      </ThemeProvider>
+    </StrictMode>
+  );
+}
