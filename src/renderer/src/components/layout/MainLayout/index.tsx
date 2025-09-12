bbeebd0910501c8e27247044bd548f454e67da06
Andre Ruan <andreruan80@gmail.com>
2025-09-12 02:01:18 -0300

    Refactor: Melhoria na estrutura do componente MainLayout para utilizar Box do MUI e gerenciar a largura da sidebar

--- src/renderer/src/components/layout/MainLayout/index.tsx
+++ src/renderer/src/components/layout/MainLayout/index.tsx
@@ -1,14 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../Sidebar";
+import { useSidebarStore } from "../../../store/sidebarStore";
+import { Box } from "@mui/material";
 
 const MainLayout = () => {
+  const isOpen = useSidebarStore((state) => state.isOpen);
+
+  const sidebarOpenWidth = 245;
+  const sidebarClosedWidth = 80;
+
+  const currentSidebarWidth = isOpen ? sidebarOpenWidth : sidebarClosedWidth;
+
   return (
-    <div style={{ display: "flex" }}>
+    <Box>
       <Sidebar />
-      <main style={{ flex: 1, padding: "1.5rem" }}>
+      <Box
+        component="main"
+        sx={{
+          marginLeft: `${currentSidebarWidth}px`,
+          padding: "1.5rem",
+          transition: "margin-left 0.2s ease-in-out",
+        }}
+      >
         <Outlet />
-      </main>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
