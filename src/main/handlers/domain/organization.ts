4b6814edb5852eec3f3c77c78a0523d64701d274
Andre Ruan <andreruan80@gmail.com>
2025-09-12 01:50:15 -0300

    Feat: Adição de handler para buscar a quantidade de logs inseridos pelo tour

--- src/main/handlers/domain/organization.ts
+++ src/main/handlers/domain/organization.ts
@@ -1,10 +1,19 @@
 import { ipcMain } from "electron";
-import { deleteAllLogs, deleteLogById, getLogs } from "../../services/organizationLogsService";
+import {
+  deleteAllLogs,
+  deleteLogById,
+  getLogs,
+  getLogsFromTourCount,
+} from "../../services/domain/organizationLogsService";
 import { db } from "../../../db";
 
 export function registerOrganizationHandlers() {
   ipcMain.handle("get-logs", async (_e, lastId?: number) => {
     return await getLogs(db, lastId);
+  });
+
+  ipcMain.handle("get-logs-from-tour-count", async () => {
+    return await getLogsFromTourCount(db);
   });
 
   ipcMain.handle("delete-log-by-id", async (_e, id: number) => {
